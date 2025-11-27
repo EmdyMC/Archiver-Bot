63c11f0d19e9af0d2ba46cc03df0e776940553c4
Emdy-MC <emdymc@gmail.com>
2025-11-27 22:27:24 +0530

    Fix command description

--- commands.py
+++ commands.py
@@ -219,7 +219,7 @@ async def pin_message(interaction: discord.Interaction, message: discord.Message
 
 # Archived designer command
 @bot.tree.command(name="archived_designer", description="Bestow the archived designer role on someone")
-@app_commands.describe(user_id="The designer's ID")
+@app_commands.describe(user_id_str="The designer's ID")
 @app_commands.checks.has_any_role(*HIGHER_ROLES)
 async def archived_designer(interaction: discord.Interaction, user_id_str: str):
     try:
