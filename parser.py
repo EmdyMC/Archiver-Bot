ed69666cd14693d2d6ef167c679dcd90fb47cec1
xTektonic <109564650+xTektonic@users.noreply.github.com>
2026-03-26 19:26:38 -0400

    Updated schema defaults

--- parser.py
+++ parser.py
@@ -694,7 +694,7 @@ message_parse_schema = dict_postprocess_parse(
                 "lag_info",
                 parse_lag_section,  # Pass the section directly
                 required=False,),
-            SchemaItem(["Video Links", "Video Link"], "video_links", videos_parse(), required=False),
+            SchemaItem(["Video Links", "Video Link"], "video_links", videos_parse(), required=False, default=[]),
             SchemaItem(
                 ["Files"],
                 "files",
@@ -704,25 +704,25 @@ message_parse_schema = dict_postprocess_parse(
                         SchemaItem(
                             ["Schematic", "Schematics"],
                             "schematics",
-                            lambda data: files_from_nodes(parse_nested_list(data)),
+                            lambda data: files_from_nodes(parse_nested_list(data))
                         ),
                         SchemaItem(
                             ["World Download", "World Downloads"],
                             "world_downloads",
                             lambda data: files_from_nodes(parse_nested_list(data)),
-                            required=False,
+                            required=False, default=[]
                         ),
                         SchemaItem(
                             ["Image", "Images"],
                             "images",
-                            lambda data: files_from_nodes(parse_nested_list(data)),
+                            lambda data: files_from_nodes(parse_nested_list(data))
                         ),
                     ],
                 )),
             SchemaItem(["Description"], "description", lambda data: serialize_nodes(parse_nested_list(data))),
-            SchemaItem(["Positives"], "positives", lambda data: serialize_nodes(parse_nested_list(data)), required=False),
-            SchemaItem(["Negatives"], "negatives", lambda data: serialize_nodes(parse_nested_list(data)), required=False),
-            SchemaItem(["Design Specifications"], "design_specifications", lambda data: serialize_nodes(parse_nested_list(data)), required=False),
+            SchemaItem(["Positives"], "positives", lambda data: serialize_nodes(parse_nested_list(data)), required=False, default=[]),
+            SchemaItem(["Negatives"], "negatives", lambda data: serialize_nodes(parse_nested_list(data)), required=False, default=[]),
+            SchemaItem(["Design Specifications"], "design_specifications", lambda data: serialize_nodes(parse_nested_list(data)), required=False, default=[]),
             SchemaItem(["Instructions"], "instructions", schema_dict_parse(
                 prefix_dict_parse("### "),
                 [
@@ -743,7 +743,7 @@ message_parse_schema = dict_postprocess_parse(
                         required=False,default=[]),
                 ],
             ), required=False),
-            SchemaItem(["Figures"], "figures", figures_parse(), required=False),
+            SchemaItem(["Figures"], "figures", figures_parse(), required=False, default=[]),
         ],
     ),
 )
